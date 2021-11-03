0ce444995e8159b8ccd70e0d5f7338a46d7be412
형준혁 <gudwnsgur12@naver.com>
2021-11-03 21:07:37 +0900

    [ADD] Spring Basic - Member Service : 비즈니스 로직 개발

---
+++ src/main/java/study/spring/basic/service/MemberService.java
@@ -0,0 +1,46 @@
+package study.spring.basic.service;
+
+import study.spring.basic.domain.Member;
+import study.spring.basic.repository.MemberRepository;
+import study.spring.basic.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        // 같은 이름이 있는 중복 회원 X
+        validateDuplicateMember(member);  // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // 중복 회원 검증 Method
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 한명 조회
+     */
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+
+}
