252523db2f1ffd0ccde66085ad40cc9a6ddfa111
형준혁 <gudwnsgur12@naver.com>
2021-11-03 19:29:12 +0900

    [ADD] Spring Basic - 2. MVC & 템플릿 엔진

--- src/main/java/study/spring/basic/controller/HelloController.java
+++ src/main/java/study/spring/basic/controller/HelloController.java
@@ -1,8 +1,10 @@
 package study.spring.basic.controller;
 
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -10,5 +12,11 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(required = false, value = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
