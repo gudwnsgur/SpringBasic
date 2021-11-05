0f835f6d534626a1c88f4560d705016a795c4602
형준혁 <gudwnsgur12@naver.com>
2021-11-05 14:46:31 +0900

    [ADD] Spring Basic - 회원 등록 기능

--- src/main/java/study/spring/basic/controller/MemberController.java
+++ src/main/java/study/spring/basic/controller/MemberController.java
@@ -2,6 +2,9 @@ package study.spring.basic.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import study.spring.basic.domain.Member;
 import study.spring.basic.service.MemberService;
 
 
@@ -28,4 +31,18 @@ public class MemberController {
 //    public void setMemberService(MemberService memberService) {
 //        this.memberService = memberService;
 //    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
+
 }
